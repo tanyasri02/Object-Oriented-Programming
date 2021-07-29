25c2e6ad803232cf7bc7e376dcdb78559f3d5c26
tanya srivastava <59734200+tanyasri02@users.noreply.github.com>
2021-07-29 20:33:02 +0530

    Create Constructor and destructor.cpp

---
+++ Constructor and destructor.cpp
@@ -0,0 +1,24 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// A Function to generate a unique OTP everytime
+class A
+{
+    public:
+    A()
+    {
+        cout<<"constructor"<<endl;
+    }
+    ~A()
+    {
+        cout<<"destructor"<<endl;
+    }
+};
+
+int main()
+{
+    A ob1;
+    
+    return 0;
+}
